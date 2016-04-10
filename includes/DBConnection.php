634d770092124e2782f52ff1f761971e25120583
Akshay195 <akshay1951996@gmail.com>
2016-04-11 01:07:34 +0530

    error solved

--- includes/DBConnection.php
+++ includes/DBConnection.php
@@ -1,11 +1,11 @@
 <?php  
 	//$conn = mysqli_connect('localhost', 'root', '','schedulingv4');
 
-	$conn = mysqli_connect('localhost', 'root', 'last1');
+	$conn = mysqli_connect('localhost', 'root', '');
 
 	 if (!$conn)
     {
-	 die('Could not connect: ' . mysqli_error());
+	 die('Could not connect: ' . mysqli_error($conn));
 	}
 	//echo 'Connected successfully' . 'iancuello';
 	mysqli_select_db( $conn,"last1");
